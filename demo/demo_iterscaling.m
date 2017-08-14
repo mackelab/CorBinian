18382ba64540c1aa4126b4f5c5fa820d9ac4bce7
mnonnenm <mnonnenm@uos.de>
2017-08-14 14:55:49 +0200

    Another pass over demo for iterScaling code.

--- demo/demo_iterscaling.m
+++ demo/demo_iterscaling.m
@@ -1,45 +1,17 @@
 
 % Simulation setup
 %--------------------------------------------------------------------------
-folder = '.';
-fname = 'tmp';
-fnames = [folder, 'tmp.mat'];
 d=60;                  % data dimensionality
 nSamplesData  = 10000; % draw from ground-truth parameters
 nSamplesEval  = 10000; % draw from paramter estimates for comparison
 burnIn        = 1000;  
-tau  = Inf;
-eps = []; % loads defaults
-beta = 0.00001*ones(d*(d+1)/2 + d+1,1);
-hJV = [true;true;true];
-ifSave = false;
-ifVK = true;
-ifbwVK = true;
 
-modelTrue = 'ising_count_l_0'; % full K-pairwise model
-modelFit  = 'ising_count_l_0'; % 
 trainMethod = 'iterativeScaling';
-
 
 h=0.25*randn(d,1)-3.5; %generate random bias terms;
 J= (3)*0.15*(randn(d)); J=triu(J,1); 
 lambda=hJ2lambda(h,J);
-switch modelTrue
-    case 'ising_count_l_0'
-      V=zeros(d+1,1);
-      V(1:28) = [3.7858,1.3932,-0.1493,-1.0014,-1.2690,-1.2713, ...
-                -1.2733,-1.0631,-0.7464,-0.5362,-0.2728,-0.0624, ...
-                 0.2010,0.4114,0.5686,0.9383,1.0423,1.1465,1.0380,...
-                 1.0359,0.9275,0.7128,0.4983,-0.1944,-0.0903, ...
-                -0.0925,-1.4757,-0.5218];
-      V(29:end) = -5; 
-      V = V(1:d+1);
-    case 'ising_count'
-     V=randn(d,1);
-    case 'ising'
-     V = [];
-end
-V = V - V(1);
+V = [0, linspace(3.5, -1, d)];
 lambdaTrue = [lambda;V];
 
 pars.d = d; 
@@ -64,8 +36,8 @@ pars.burnIn = burnIn;
  
  lambdaInd = zeros(size(lambdaTrue)); 
  lambdaInd(1:d) = log(EX./(1-EX));
- lambdaInd(lambdaInd==Inf) =   1000; % fairly hacky solution in case
- lambdaInd(lambdaInd==-Inf) = -1000; % EX(k) = 0 resp. EX(k) = 1
+ lambdaInd(lambdaInd==Inf) =   1000; % catch cases EX(k)=0 resp. EX(k)=1
+ lambdaInd(lambdaInd==-Inf) = -1000; % 
  fitoptions.lambda0 = lambdaInd;
  clear EX tmp xTrain fxTrain 
 
@@ -84,34 +56,43 @@ switch trainMethod
     lambdaHat = -lambdaHat;
     
   case 'iterativeScaling'
+          
+    hJV = ones(3,1); % booleans giving whether to include h, J and/or V 
+    ifbwVK = true;   % flag for blockwise update of parameter terms V
+    
+    ifSave = false;  % whether to store results on disk
+    fname = '';      % filename for storing results on disk
+
       
     fitoptions.regular = 'l1';
+    beta = 0.00001*ones(d*(d+1)/2 + d+1,1); % strength of l1 regularizer
     fitoptions.machine  = 'cluster';
     fitoptions.nRestart = 1;
-    fitoptions.modelFit = modelFit;
+    fitoptions.modelFit = 'ising_count_l_0';
 
-    fitoptions.nSamples = 10000;    
-    fitoptions.burnIn  = 1000;
-    fitoptions.maxIter = 1000;
+    fitoptions.nSamples = 500;    
+    fitoptions.burnIn   = 100;
+    fitoptions.maxIter  = 2000;
     fitoptions.maxInnerIter = 1;        
+    eps = []; % convergence criteria, empty loads defaults
     
-    a = fitoptions.nSamples;
+    % make sequence of increasing MCMC chain lengths for each update step
+    a = fitoptions.nSamples; % initial MCMC chain lengths
+    tau = 1000;              % update steps over which chain length doubles
     fitoptions.nSamples = [0;round(a * 2.^((1:fitoptions.maxIter)'/tau))];
     fitoptions.nSamples = floor(fitoptions.nSamples/100)*100;          
-    
+        
     [lambdaHat, fitDiagnostics] = iterScaling(mfxTrain, fitoptions, ...
-                              beta, eps, fname, ifSave, hJV, ifVK, ifbwVK);
+                              beta, eps, fname, ifSave, hJV, ifbwVK);
 end
 
 
 % validate model
 %--------------------------------------------------------------------------
- disp('Generating data from model fit')
- [mfxEval,~,~] = maxEnt_gibbs_pair_C(nSamplesEval, burnIn, ...
-                                     lambdaHat, x0, fitoptions.machine);
 
+% for small systems, we can compute P( X | lambdaTrue ) analytically                                 
 if d < 20
- [features,description,x]=setup_features_maxent(d,modelTrue);
+ [features,description,x]=setup_features_maxent(d,'ising_count_l_0');
  [~,~,Ptrue, ~]=logPMaxEnt(features,lambdaTrue);
  EX = sum(bsxfun(@times, x', Ptrue'),2);
  description(isnan(description)) = d+1;
@@ -120,42 +101,40 @@ if d < 20
  EXX = sum(bsxfun(@times, (x1(:,description(1,d+1:d*(d+1)/2))...
                         .* x1(:,description(2,d+1:d*(d+1)/2)))',Ptrue'),2);
  EK = zeros(length(L),1);
- switch modelTrue
-    case 'ising_count'
-     for k = 1:length(EK)
-      EK(k) = sum((sum(x,2)==(k)) .* Ptrue);
-     end
-    case 'ising_count_l_0'
-     for k = 1:length(EK)
-      EK(k) = sum((sum(x,2)==(k-1)) .* Ptrue);
-     end
+ for k = 1:length(EK)
+  EK(k) = sum((sum(x,2)==(k-1)) .* Ptrue);
  end
- mfxTrue = [EX(:);EXX(:);EK(:)];
+ mfxEval = [EX(:);EXX(:);EK(:)];
  clear x1 EX EXX EK description features Ptrue 
+
+else % for large systems, sample (long) MCMC chain
+
+ disp('Generating data from model fit')
+ [mfxEval,~,~] = maxEnt_gibbs_pair_C(nSamplesEval, burnIn, ...
+                                     lambdaHat, x0, fitoptions.machine);
+    
 end % if d < 20  
                     
-if ~exist('mfxTrue', 'var')
-    mfxTrue = [];
-end
-%%
+% visualize results
 figure; 
 subplot(131)
-plot(mfxTrue(1:d), mfxEval(1:d), '.');
-hold on
 plot(mfxTrain(1:d), mfxEval(1:d), 'k.');
 title('first moments')
+xlabel('est.')
+ylabel('data')
 axis square
 subplot(132)
-plot(mfxTrue(d+1:end-d-1), mfxEval(d+1:end-d-1), '.')
-hold on
 plot(mfxTrain(d+1:end-d-1), mfxEval(d+1:end-d-1), 'k.')
 title('second moments')
+xlabel('est.')
+ylabel('data')
 axis square
 subplot(133)
 plot(mfxTrain(end-d:end), 'ko-')
 hold on
-plot(mfxTrue(end-d:end), 'o-')
 plot(mfxEval(end-d:end), 'ro-')
 hold off
-legend({'data', 'true', 'est.'})
+xlabel('population count K')
+ylabel('probability P(K)')
+legend({'data', 'est.'})
 title('population counts')
