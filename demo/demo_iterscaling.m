3da45d3a98ffb8116b90241b65986ad47a9ec3ea
mnonnenm <mnonnenm@uos.de>
2017-08-14 11:05:41 +0200

    Pass over demo function for iterative scaling functionality.

--- demo/demo_iterscaling.m
+++ demo/demo_iterscaling.m
@@ -4,16 +4,10 @@
 folder = '.';
 fname = 'tmp';
 fnames = [folder, 'tmp.mat'];
-d=15;                  % data dimensionality
+d=60;                  % data dimensionality
 nSamplesData  = 10000; % draw from ground-truth parameters
 nSamplesEval  = 10000; % draw from paramter estimates for comparison
 burnIn        = 1000;  
-fitoptions.nSamples = 10000;    
-fitoptions.burnIn  = 1000;
-fitoptions.maxIter = 1000;
-fitoptions.maxInnerIter = 1;    
-useLambdaInd = true; 
-a = fitoptions.nSamples;
 tau  = Inf;
 eps = []; % loads defaults
 beta = 0.00001*ones(d*(d+1)/2 + d+1,1);
@@ -21,18 +15,11 @@ hJV = [true;true;true];
 ifSave = false;
 ifVK = true;
 ifbwVK = true;
-fitoptions.nSamples = [0;round( a * 2.^((1:fitoptions.maxIter)' / tau ))];
-fitoptions.nSamples = floor(fitoptions.nSamples/100)*100;
 
 modelTrue = 'ising_count_l_0'; % full K-pairwise model
 modelFit  = 'ising_count_l_0'; % 
 trainMethod = 'iterativeScaling';
 
-% iterative scaling-related
-fitoptions.regular = 'l1';
-fitoptions.machine  = 'cluster';
-fitoptions.nRestart = 1;
-fitoptions.modelFit = modelFit;
 
 h=0.25*randn(d,1)-3.5; %generate random bias terms;
 J= (3)*0.15*(randn(d)); J=triu(J,1); 
@@ -52,10 +39,8 @@ switch modelTrue
     case 'ising'
      V = [];
 end
- V = V - V(1);
- lambdaTrue = [lambda;V];
-
-newData = true;
+V = V - V(1);
+lambdaTrue = [lambda;V];
 
 pars.d = d; 
 pars.beta = beta; 
@@ -63,25 +48,17 @@ pars.nSamplesData = nSamplesData;
 pars.nSamplesEval  = nSamplesEval;
 pars.burnIn = burnIn;
 
-compTimes = zeros(length(nSamplesData),2,6);
-
-for r = 1:length(nSamplesData)
-
-compTimes(r,1,:) = clock;
-
 
 % generate training data
 %--------------------------------------------------------------------------
-  % Initialize training data-generating MCMC chain with a sample drawn from
-  % a nested model (only h = lamdbdaTrue(1:d), i.e. no J, no L)
-  EX = exp(lambdaTrue(1:d))./(1+exp(lambdaTrue(1:d))); % works ok for small
-  x0 = double(rand(d,1)<EX);                           % entries of L,J
+% Initialize training data-generating MCMC chain with a sample drawn from
+% a nested model (only h = lamdbdaTrue(1:d), i.e. no J, no L)
+ EX = exp(lambdaTrue(1:d))./(1+exp(lambdaTrue(1:d))); % works ok for small
+ x0 = double(rand(d,1)<EX);                           % entries of L,J
 
-  if newData % else we already have mfxTrain
-  disp('Generating training data')
-  [mfxTrain,~,~] = maxEnt_gibbs_pair_C(nSamplesData(r), burnIn(r), ...
-                                       lambdaTrue, x0, fitoptions.machine);
-  end
+ disp('Generating training data')
+ [mfxTrain,~,~] = maxEnt_gibbs_pair_C(nSamplesData, burnIn, ...
+                                   lambdaTrue, x0, fitoptions.machine);
 
  EX = mfxTrain(1:d);
  
@@ -89,10 +66,7 @@ compTimes(r,1,:) = clock;
  lambdaInd(1:d) = log(EX./(1-EX));
  lambdaInd(lambdaInd==Inf) =   1000; % fairly hacky solution in case
  lambdaInd(lambdaInd==-Inf) = -1000; % EX(k) = 0 resp. EX(k) = 1
-
- if useLambdaInd 
-   fitoptions.lambda0 = lambdaInd;
- end
+ fitoptions.lambda0 = lambdaInd;
  clear EX tmp xTrain fxTrain 
 
 
@@ -111,6 +85,20 @@ switch trainMethod
     
   case 'iterativeScaling'
       
+    fitoptions.regular = 'l1';
+    fitoptions.machine  = 'cluster';
+    fitoptions.nRestart = 1;
+    fitoptions.modelFit = modelFit;
+
+    fitoptions.nSamples = 10000;    
+    fitoptions.burnIn  = 1000;
+    fitoptions.maxIter = 1000;
+    fitoptions.maxInnerIter = 1;        
+    
+    a = fitoptions.nSamples;
+    fitoptions.nSamples = [0;round(a * 2.^((1:fitoptions.maxIter)'/tau))];
+    fitoptions.nSamples = floor(fitoptions.nSamples/100)*100;          
+    
     [lambdaHat, fitDiagnostics] = iterScaling(mfxTrain, fitoptions, ...
                               beta, eps, fname, ifSave, hJV, ifVK, ifbwVK);
 end
@@ -119,7 +107,7 @@ end
 % validate model
 %--------------------------------------------------------------------------
  disp('Generating data from model fit')
- [mfxEval,~,~] = maxEnt_gibbs_pair_C(nSamplesEval(r), burnIn(r), ...
+ [mfxEval,~,~] = maxEnt_gibbs_pair_C(nSamplesEval, burnIn, ...
                                      lambdaHat, x0, fitoptions.machine);
 
 if d < 20
@@ -132,7 +120,7 @@ if d < 20
  EXX = sum(bsxfun(@times, (x1(:,description(1,d+1:d*(d+1)/2))...
                         .* x1(:,description(2,d+1:d*(d+1)/2)))',Ptrue'),2);
  EK = zeros(length(L),1);
-switch modelTrue
+ switch modelTrue
     case 'ising_count'
      for k = 1:length(EK)
       EK(k) = sum((sum(x,2)==(k)) .* Ptrue);
@@ -141,15 +129,11 @@ switch modelTrue
      for k = 1:length(EK)
       EK(k) = sum((sum(x,2)==(k-1)) .* Ptrue);
      end
-end
-mfxTrue = [EX(:);EXX(:);EK(:)];
-clear x1 EX EXX EK description features Ptrue 
+ end
+ mfxTrue = [EX(:);EXX(:);EK(:)];
+ clear x1 EX EXX EK description features Ptrue 
 end % if d < 20  
                     
-compTimes(r,2,:) = clock;
-
-end % end for r = 1:length(nSamplesData)
-
 if ~exist('mfxTrue', 'var')
     mfxTrue = [];
 end
