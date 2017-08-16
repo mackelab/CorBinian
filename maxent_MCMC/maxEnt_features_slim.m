b4565220ff29748ddfb3985779d359e7afd966c6
mnonnenm <mnonnenm@uos.de>
2017-08-16 14:43:29 +0200

    Adding documentation - in particular the inputs of maxEnt_features_slim needed better description,   as the function is meant ot be used on one data vector at a time.

--- maxent_MCMC/maxEnt_features_slim.m
+++ maxent_MCMC/maxEnt_features_slim.m
@@ -1,4 +1,26 @@
 function fx = maxEnt_features_slim(x, model)
+% calculature features for max-ent model fitting
+
+
+% first argument: the vector to which the feature maps should be supplied
+% inputs: 
+%  x: a binary vector of length d
+% map: either integers 1, 2, 3, ('first', 'second', 'third-order' maxent),
+% or 'ising' (same as 2) or 'ising_count' for ising model with additional 
+% constraints on the total activity count
+% or a function handle for using user-supplied feature maps
+%
+%outputs:
+% fvals, the calculated features. If map=1, then same as x, if map=2, this
+% is a copy of x concatenated with all two-tupels without repetion, if
+% map=3, then this is further concatenated with all three-tupels without
+% any repetions. (Note that this is NOT what you might want if x is
+% different from a binary (0,1) repetiation, as it excludes all the x.^2,
+% and x.^3 features etc, as they are redundant in this representation!
+
+% function adapted from the pop_spike code base 
+% https://bitbucket.org/mackelab/pop_spike
+
  d = length(x);
  switch model
     case {2,'ising'}
